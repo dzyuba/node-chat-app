01aacbb7cf7b09b31128f50d014461ec6205715b
dzyuba <gruberhoff@gmail.com>
2018-04-25 15:59:23 +0300

    Add form for messages and show incoming

--- server/server.js
+++ server/server.js
@@ -16,18 +16,8 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('createMessage', (message) => {
-
-        socket.emit('newMessage', {
-            from: "Admin",
-            text: "Welcome to the chat app"
-        });
-
-        socket.broadcast.emit('newMessage', {
-            from: "Admin",
-            text: "New user joined",
-            createdAt: new Date().getTime()
-        });
+    socket.on('createMessage', (message, callback) => {
+        console.log('createMessage', message);
 
         // push message to all connections (users)
         io.emit('newMessage', {
@@ -35,6 +25,8 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        callback('This is from the server.');
     });
 
     socket.on('disconnect', () => {
