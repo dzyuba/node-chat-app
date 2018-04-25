8dc29b01b7d7f79d29b21cca65072a5bf02ceac5
dzyuba <gruberhoff@gmail.com>
2018-04-25 17:08:41 +0300

    Added getting user's geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -27,3 +27,17 @@ $('#message-form').on('submit', function(e) {
 
     });
 });
+
+var locationButton = $('#send-location');
+
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+    }, function(e) {
+        alert('Unable to get location', e);
+    });
+});
